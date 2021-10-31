00eabe29f9961368472f1780ea2c5f7cc95d0dbe
ShadowCommander <10494922+ShadowCommander@users.noreply.github.com>
2021-10-31 10:58:46 +0100

    Added short descriptions to iter functions

--- libft_list.h
+++ libft_list.h
@@ -6,7 +6,7 @@
 /*   By: jtong <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/04 18:55:55 by jtong             #+#    #+#             */
-/*   Updated: 2021/10/31 10:55:44 by jtong            ###   ########.fr       */
+/*   Updated: 2021/10/31 10:58:14 by jtong            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,7 +50,9 @@ t_node	*ft_listlop(t_llist *list);
 t_node	*ft_listrm(t_node *prev, t_node *node);
 t_node	*ft_listsrt(t_node **next, t_node *node);
 
+/* Calls a function on every node */
 void	ft_nodeiter(t_node *node, void (*f)(t_node *node));
+/* Calls a function on every node in a list after the specified node */
 void	ft_listfiter(t_llist *list, t_node *node,
 		void (*f)(t_llist *list, t_node *node))
 
