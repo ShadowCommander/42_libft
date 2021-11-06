b4827f86fc27fed764f808148769bfe010923473
ShadowCommander <10494922+ShadowCommander@users.noreply.github.com>
2021-11-06 05:57:26 +0100

    Fixed ft_strtol.c

--- ft_strtol.c
+++ ft_strtol.c
@@ -6,7 +6,7 @@
 /*   By: jtong <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/01 14:12:37 by jtong             #+#    #+#             */
-/*   Updated: 2021/11/06 05:18:08 by jtong            ###   ########.fr       */
+/*   Updated: 2021/11/06 05:56:54 by jtong            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,29 +26,28 @@ static int	ft_ctoi(int c)
 
 long	ft_strtol(const char *ptr, char **endptr, int base)
 {
-	t_strtol	vars;
+	t_strtol	v;
 
 	if (base <= 1 || base > 16)
 		return (0);
-	vars = (t_strtol){ptr, 0, 1, 0, 0, 0};
-	while (ft_isspace(*vars.s))
-		vars.s++;
-	if (ft_isalnum(vars.s[1]) && (*vars.s != '+' && *vars.s++ == '-'))
-		vars.neg = -1;
-	vars.cutoff = (LONG_MAX * vars.neg) / base;
-	vars.cutlim = (LONG_MAX * vars.neg) % base;
-	while (ft_isalnum(*vars.s))
+	v = (t_strtol){ptr, 0, 1, 0, 0, 0};
+	while (ft_isspace(*v.s))
+		v.s++;
+	if (*v.s == '-' && ft_isalnum(v.s[1]))
+		v.neg = -1;
+	if ((*v.s == '-' || *v.s == '+') && ft_isalnum(v.s[1]))
+		v.s++;
+	v.cutoff = (LONG_MAX * v.neg) / base;
+	v.cutlim = (LONG_MAX * v.neg) % base;
+	while (ft_isalnum(*v.s) && ft_ctoi(*v.s) < base)
 	{
-		vars.c = ft_ctoi(*vars.s);
-		if (vars.c < base)
-			break ;
-		if (vars.acc >= vars.cutoff * vars.neg
-			&& vars.c > vars.cutlim + (vars.neg == -1))
-			return (LONG_MAX * vars.neg);
-		vars.acc = (vars.acc * base) + (vars.c * vars.neg);
-		vars.s++;
+		v.c = ft_ctoi(*v.s);
+		if (v.acc >= v.cutoff * v.neg && v.c > v.cutlim + (v.neg == -1))
+			return (LONG_MAX * v.neg);
+		v.acc = (v.acc * base) + (v.c * v.neg);
+		v.s++;
 	}
 	if (endptr)
-		*endptr = (char *)(vars.s);
-	return (vars.acc);
+		*endptr = (char *)(v.s);
+	return (v.acc);
 }
